3edfecdcfd2c147e7c2ff524f32ef996a0d859d7
RedIrishTACK <anthonygildhouse@yahoo.com>
2015-09-30 15:51:47 -0500

    week seven run down
    week seven run down

---
+++ Assets/scripts/MoveTo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+	public Transform goal;
+	NavMeshAgent agent;
+	
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+
+	}
+
+	void Update () {
+		agent.destination = goal.position;
+	}
+}
